14f8ab208a46198c5da483a33ee87f48bae6f29c
superdoggo83 <bramvandersluis83@gmail.com>
2025-05-23 11:30:11 +0200

    fixed routes file structure

---
+++ VueADW/src/router/routes.js
@@ -0,0 +1,47 @@
+import InboxPage from '../pages/InboxPage.vue'
+import HomePage from '../pages/HomePage.vue'
+import ContactDetail from '@/pages/ContactDetail.vue'
+import LoginPage from '@/pages/LoginPage.vue'
+import Dienst from '@/pages/DienstPage.vue'
+import ContactPage from '@/pages/ContactPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'HomePage',
+    component: HomePage
+  },
+  {
+    path: '/InboxPage',
+    name: 'inbox',
+    component: InboxPage
+  },
+   {
+    path: '/contacts/:id',
+    name: 'ContactDetail',
+    component: ContactDetail
+  },
+   {
+    path: '/LoginPage',
+    name: 'Login',
+    component: LoginPage
+  },
+  {
+    path: '/dienst',
+    name: 'dienst',
+    component: Dienst 
+  },
+   {
+    path: '/ContactPage',
+    name: 'ContactPage',
+    component: ContactPage
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
+
