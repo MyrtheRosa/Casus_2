63dab60d5455c09a9aac677e165be841ee197278
MyrtheRosa <myrthe-willemsen@hotmail.com>
2025-05-26 09:03:59 +0200

    privacy policy V1.0

--- VueADW/src/router/routes.js
+++ VueADW/src/router/routes.js
@@ -4,11 +4,14 @@ import { createRouter, createWebHistory } from "vue-router";
 const HomePage = () => import("../pages/HomePage.vue");
 const LoginPage = () => import("../pages/LoginPage.vue");
 const ContactPage = () => import("../pages/ContactPage.vue");
+const PrivacyPolicy = () => import('../pages/PrivacyPolicy.vue');
 
 const routes = [
   { path: "/", name: "Home", component: HomePage },
   { path: "/Login", name: "Login", component: LoginPage },
   { path: "/Contact", name: "Contact", component: ContactPage },
+  { path: '/PrivacyPolicy', name: "Privacy", component: PrivacyPolicy},
+  
   // Add more routes as needed
 ]
 
