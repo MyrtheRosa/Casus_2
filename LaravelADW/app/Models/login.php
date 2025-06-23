0051a41a74a9a47dc96d269bcb7af3455f47616a
superdoggo83 <bramvandersluis83@gmail.com>
2025-06-23 09:41:20 +0200

    started adding sanctum

--- LaravelADW/app/Models/login.php
+++ LaravelADW/app/Models/login.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class login extends Model
+
+class login extends Authenticatable
 {
     use HasFactory;
+    use HasApiTokens;
     protected $table ='login';
-    protected $fillable = ['email', 'password'];
+    protected $fillable = ['email', 'password', 'is_admin'];
 }
